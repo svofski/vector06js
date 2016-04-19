fc3267e2c4f4246e2714f663e3a319ee23ed303d
svo <svofski@gmail.com>
2016-04-19 22:23:03 +0300

    support archives with multiple entries

--- rom.js
+++ rom.js
@@ -4,7 +4,7 @@
 // Load ROM from url: url can be direct rom/r0m, or an url of a zip
 // file containing a rom/r0m. First suitable entry will be used.
 //
-function Loader(url, callback, callback_error, callback_fdd) {
+function Loader(url, callback, callback_error, callback_fdd, parent_id, container_id) {
     var fetchROM2 = function(url, callback, callback_error) {
         var oReq = new XMLHttpRequest();
         oReq.open("GET", url, true);
@@ -15,9 +15,9 @@ function Loader(url, callback, callback_error, callback_fdd) {
             tryUnzip(url, blob, callback);
         };
         oReq.onerror = function(oEvent) {
-        	console.log("XMLHttpRequest error", oEvent);
-        	callback_error();
-        }        
+            console.log("XMLHttpRequest error", oEvent);
+            callback_error();
+        }
 
         oReq.send();
     }
@@ -31,31 +31,93 @@ function Loader(url, callback, callback_error, callback_fdd) {
     }
 
     var extract = function(entry, callback, start) {
+        console.log("Launching ", entry.filename);
         writer = new zip.BlobWriter("application/octet-stream");
         entry.getData(writer, function(data) {
             readData(data, callback, start);
         });
     }
 
+    var extractAndLaunch = function(entry) {
+        var lower = entry.filename.toLowerCase();
+        if (lower.endsWith("rom") ||
+            lower.endsWith("r0m") ||
+            lower.endsWith("bin") ||
+            lower.endsWith("com")) {
+            var start = lower.endsWith("r0m") || lower.endsWith("bin") ? 0 : 0x100;
+            extract(entry, callback, start);
+        } else {
+            if (lower.endsWith("fdd")) {
+                extract(entry, callback_fdd, 0);
+            }
+        }
+    };
+
+    var isRom = function(name) {
+        var lower = name.toLowerCase();
+        return lower.endsWith("rom") || lower.endsWith("r0m") || lower.endsWith("com") ||
+            lower.endsWith("bin");
+    };
+    var isFdd = function(name) {
+        var lower = name.toLowerCase();
+        return lower.endsWith("fdd");
+    };
+
+    var createChooser = function(items) {
+        var parent = document.getElementById(parent_id);
+        var container = document.getElementById(container_id);
+
+        var ol = document.createElement("ul");
+        ol.className = "romchooser-ol";
+        container.appendChild(ol);
+        container.style.height = (parent.parentNode.clientHeight - parent.parentNode.clientHeight * 0.3) + "px";
+        for (var i = 0; i < items.length; i += 1) {
+            var li = document.createElement("li");
+            var a = document.createElement("a");
+            a.href = "#";
+            li.appendChild(a);
+            a.innerText = items[i].filename;
+            (function(clickitem) {
+                li.onclick = function() {
+                    parent.style.display = "none"
+                    extractAndLaunch(clickitem);
+                }
+            })(items[i]);
+            if (isRom(items[i].filename)) {
+                li.className = "chooser-li-rom";
+            } else if (isFdd(items[i].filename)) {
+                li.className = "chooser-li-fdd";
+            } else {
+                li.className = "chooser-li-wtf";
+            }
+
+
+            ol.appendChild(li);
+
+        }
+
+        parent.style.display = "table";
+    };
+
     var tryUnzip = function(url, blob, callback) {
         zip.createReader(new zip.BlobReader(blob), function(reader) {
                 reader.getEntries(function(entries) {
                     if (entries.length) {
+                        var validlist = [];
                         for (var i = 0; i < entries.length; i++) {
                             var lower = entries[i].filename.toLowerCase();
                             if (lower.endsWith("rom") ||
                                 lower.endsWith("r0m") ||
-                                lower.endsWith("com")) {
-                                var start = lower.endsWith("r0m") ? 0 : 0x100;
-                                console.log("Found ROM: ", entries[i].filename,
-                                    "start=", start.toString(16));
-                                extract(entries[i], callback, start);
-                                break;
-                            } else {
-                                if (lower.endsWith("fdd")) {
-                                    extract(entries[i], callback_fdd, 0);
-                                }
+                                lower.endsWith("com") ||
+                                lower.endsWith("bin") ||
+                                lower.endsWith("fdd")) {} {
+                                validlist.push(entries[i]);
                             }
+                        }
+                        if (validlist.length == 1) {
+                            extractAndLaunch(validlist[0]);
+                        } else {
+                            createChooser(validlist);
                         }
                     }
                 });
